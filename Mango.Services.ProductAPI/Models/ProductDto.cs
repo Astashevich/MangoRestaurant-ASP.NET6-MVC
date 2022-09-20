20d412ec78de8b11f20d1dc9efc6b6e63631c051
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-20 17:16:07 +0300

    Create Dto and auto mapper with config

--- Mango.Services.ProductAPI/Models/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/ProductDto.cs
@@ -2,13 +2,10 @@
 
 namespace Mango.Services.ProductAPI.Models
 {
-    public class Product
+    public class ProductDto
     {
-        [Key]
         public int ProductId { get; set; }
-        [Required]
         public int Name { get; set; }
-        [Range(1, 1000)]
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
