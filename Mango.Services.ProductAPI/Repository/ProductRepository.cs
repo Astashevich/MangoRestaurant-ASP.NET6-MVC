e46f75c8859c9975e5182124aac2034b6484a10a
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-21 13:50:38 +0300

    Create Product Repository and connect it in Program

---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            IEnumerable<Product> productList = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(productList);
+        }
+
+        public async Task<ProductDto> GetProductById(int id)
+        {
+            Product product = await _context.Products.Where(x => x.ProductId == id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            Product product = _mapper.Map<ProductDto, Product>(productDto);
+            if(product.ProductId > 0)
+            {
+                _context.Products.Update(product);
+            }
+            else
+            {
+                _context.Products.Add(product);
+            }
+            await _context.SaveChangesAsync();
+            return _mapper.Map<Product, ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int id)
+        {
+            try
+            {
+                Product product = await _context.Products.FirstOrDefaultAsync(x => x.ProductId == id);
+
+                if (product == null) return false;
+
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
