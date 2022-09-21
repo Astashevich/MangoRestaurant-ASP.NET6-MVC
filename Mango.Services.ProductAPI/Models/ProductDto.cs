275061e675d7e0a40b295b8674ca132846431899
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-21 15:44:02 +0300

    Fill DB with content

--- Mango.Services.ProductAPI/Models/ProductDto.cs
+++ Mango.Services.ProductAPI/Models/ProductDto.cs
@@ -5,7 +5,7 @@ namespace Mango.Services.ProductAPI.Models
     public class ProductDto
     {
         public int ProductId { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
