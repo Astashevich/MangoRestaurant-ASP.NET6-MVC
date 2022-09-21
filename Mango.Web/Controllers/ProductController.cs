399c78ad9d123ff159f913e9fb5a8060d48949df
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-21 18:47:57 +0300

    Create Product

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -22,5 +22,23 @@ namespace Mango.Web.Controllers
                 list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
             return View(list);
         }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
     }
 }
