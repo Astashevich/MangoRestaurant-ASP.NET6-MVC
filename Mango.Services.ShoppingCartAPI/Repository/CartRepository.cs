a0464d9736ebaadd3553cce3305ad1954a4ff2d7
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-26 15:47:30 +0300

    Add to Shopping Cart Repository Method

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -1,9 +1,21 @@
-﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Repository
 {
     public class CartRepository : ICartRepository
     {
+        private readonly ApplicationDbContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
         public async Task<CartDto> GetCartByUserId(string userId)
         {
@@ -12,7 +24,29 @@ namespace Mango.Services.ShoppingCartAPI.Repository
 
         public async Task<CartDto> CreateUpdateCart(CartDto cartDto)
         {
-            throw new NotImplementedException();
+            Cart cart = _mapper.Map<Cart>(cartDto);
+
+            //Check if product exists in database, if not create it.
+            var prodInDb = await _context.Products.FirstOrDefaultAsync(u =>
+                u.ProductId == cartDto.CartDetails.FirstOrDefault().ProductId);
+            if (prodInDb == null)
+            {
+                _context.Products.Add(cart.CartDetails.FirstOrDefault().Product);
+                await _context.SaveChangesAsync();
+            }
+
+            //Check if header is null, if not create header and details.
+            var cartHeaderFromDb = _context.CartHeader.FirstOrDefaultAsync(u =>
+                u.UserId == cart.CartHeader.UserId);
+            if (cartHeaderFromDb == null)
+            {
+                _context.CartHeader.Add(cart.CartHeader);
+                await _context.SaveChangesAsync();
+                cart.CartDetails.FirstOrDefault().CartHeaderId = cart.CartHeader.CartHeaderId;
+                cart.CartDetails.FirstOrDefault().Product = null;
+                _context.CartDetails.Add(cart.CartDetails.FirstOrDefault());
+                await _context.SaveChangesAsync();
+            }
         }
 
         public async Task<bool> RemoveFromCart(int cartDetailsId)
