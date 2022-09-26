0ec7e0b7d67d805e4432ee9253ed2961105002e2
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-26 17:05:59 +0300

    Configure Cart Service

--- Mango.Web/Services/IServices/ICartService.cs
+++ Mango.Web/Services/IServices/ICartService.cs
@@ -8,5 +8,6 @@ namespace Mango.Web.Services.IServices
         Task<T> AddToCartAsync<T>(CartDto cartDto, string token = null);
         Task<T> UpdateCartAsync<T>(CartDto cartDto, string token = null);
         Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
+        Task<T> ClearCartAsync<T>(string userId, string token = null);
     }
 }
