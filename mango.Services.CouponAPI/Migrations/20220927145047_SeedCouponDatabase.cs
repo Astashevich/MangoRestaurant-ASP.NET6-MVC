a196a672e0c94092eb3a3f4f72233b6a77155c1d
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-27 17:55:16 +0300

    Coupon Repository

---
+++ mango.Services.CouponAPI/Migrations/20220927145047_SeedCouponDatabase.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mango.Services.CouponAPI.Migrations
+{
+    public partial class SeedCouponDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CouponCode",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount" },
+                values: new object[] { 1, "10OFF", 10.0 });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount" },
+                values: new object[] { 2, "20OFF", 20.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CouponCode",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ mango.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,9 @@
+﻿using mango.Services.CouponAPI.Models.Dto;
+
+namespace mango.Services.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDto> GetCouponByCode(string couponCode);
+    }
+}
