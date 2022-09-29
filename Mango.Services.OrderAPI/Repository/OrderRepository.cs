9bbd380a5f6d9d619650bb84cc20e9f5f3e44809
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-09-29 17:28:10 +0300

    Implement Order Repository

--- Mango.Services.OrderAPI/Repository/OrderRepository.cs
+++ Mango.Services.OrderAPI/Repository/OrderRepository.cs
@@ -1,17 +1,35 @@
-﻿using Mango.Services.OrderAPI.Models;
+﻿using Mango.Services.OrderAPI.DbContexts;
+using Mango.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.OrderAPI.Repository
 {
     public class OrderRepository : IOrderRepository
     {
-        public Task<bool> AddOrder(OrderHeader orderHeader)
+        private readonly DbContextOptions<ApplicationDbContext> _context;
+
+        public OrderRepository(DbContextOptions<ApplicationDbContext> context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        public async Task<bool> AddOrder(OrderHeader orderHeader)
         {
-            throw new NotImplementedException();
+            await using var _db = new ApplicationDbContext(_context);
+            _db.OrderHeaders.Add(orderHeader);
+            _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(int orderHeaderId, bool paid)
+        {
+            await using var _db = new ApplicationDbContext(_context);
+            var orderHeaderFromDb = await _db.OrderHeaders.FirstOrDefaultAsync(u => u.OrderHeaderId == orderHeaderId);
+            if(orderHeaderFromDb == null)
+            {
+                orderHeaderFromDb.PaymentStatus = paid;
+                await _db.SaveChangesAsync();
+            }
         }
     }
 }
