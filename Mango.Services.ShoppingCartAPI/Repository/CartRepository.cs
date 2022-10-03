beae5c9aa14bcd248c20d091633bf07b6b7bbb98
Arseniy Astashevich <ars.astashevich@gmail.com>
2022-10-03 17:29:23 +0300

    Synchronous Communication and Alerts Display

--- Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
+++ Mango.Services.ShoppingCartAPI/Repository/CartRepository.cs
@@ -74,6 +74,8 @@ namespace Mango.Services.ShoppingCartAPI.Repository
                     //Update the count / cart deatails.
                     cart.CartDetails.FirstOrDefault().Product = null;
                     cart.CartDetails.FirstOrDefault().Count += cartDetailsFromDb.Count;
+                    cart.CartDetails.FirstOrDefault().CartDetailsId += cartDetailsFromDb.CartDetailsId;
+                    cart.CartDetails.FirstOrDefault().CartHeaderId += cartDetailsFromDb.CartHeaderId;
                     _context.CartDetails.Update(cart.CartDetails.FirstOrDefault());
                     await _context.SaveChangesAsync();
                 }
